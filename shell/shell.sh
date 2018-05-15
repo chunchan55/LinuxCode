5dd676299041c61cc78d3b40212ad676621e762c
chunchan55 <1946429765@qq.com>
2018-05-15 21:32:52 +0800

    add shell

---
+++ shell/shell.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#sort默认升序，-r 降序
+#cat file.txt | sort -r > data.txt.sorted
+#-u 去重
+#cat file.txt | sort -u
+#-n指定使用按照数值进行排序,默认是按照字典序
+#cat file.txt | sort -n
+#指定分隔符，指定域进行排序
+#cat file.txt | sort -n -r -k 2 -t
+#uniq 相邻行去重
+#echo "aaa
+#bbb
+#bbb
+#ccc
+#bbb" | uniq -c
+
+
