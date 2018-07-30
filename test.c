9765b6bb07557cfb190d50a8c30448c9cb4c94d7
chunchan55 <1946429765@qq.com>
2018-07-30 11:42:42 +0800

    test.c

--- test.c
+++ test.c
@@ -3,5 +3,12 @@ int main()
 {
   //code test
   printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
+  printf("hello test\n");
   return 0;
 }
